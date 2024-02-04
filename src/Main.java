acb5082e06c7811b315019b900c739192a031b62
mira1309 <134642665+mira1309@users.noreply.github.com>
2024-02-04 18:21:18 +0300

    finish

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            // ...
+            //  ...
         } catch (NullPointerException | IllegalArgumentException e) {
             System.out.println(e);
         }
